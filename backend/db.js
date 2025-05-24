898d36b85ded05b997b6d3248fb5adf358451109
Dhruvik <dhruvikkothiya732002@gmail.com>
2025-05-24 18:03:10 +0530

    update connection string

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const db=require('mongoose')
-const url='mongodb+srv://socialfox73:Dhruvik123@socialfox.7csphaz.mongodb.net/'
+const url='mongodb://socialfox73:Dhruvik123@socialfox.7csphaz.mongodb.net/?retryWrites=true&w=majority&appName=SocialFox'
 const connection = async () => {
      if (!db.connections[0].readyState) {
        await db.connect(url, {
